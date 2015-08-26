aba1539dbb857ff1ba05ef8ed2eccc27b02b85ff
servancho <bxtgroup@gmail.com>
2015-08-26 15:03:23 +0300

    Initial import

---
+++ tests/PyBillingApiTest.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * PHP Unit tests for CMDB API Php binding.
+ * (c) Dmitry Shilyaev
+ *
+ * Using tests
+ *      Start development CMDB server: ./manage.py runserver
+ */
+
+require_once dirname(dirname(__FILE__)) . '/resources/Resource.php';
+
+class PyBillingApiTest extends PHPUnit_Framework_TestCase {
+
+    public static function setUpBeforeClass() {
+    }
+
+}
